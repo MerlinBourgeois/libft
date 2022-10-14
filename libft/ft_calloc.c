233c8659e93fa46823e84a9ba8e28c129513e54f
R0uge_ <merlinbourgeois@gmail.com>
2022-10-14 12:43:42 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   libft/Makefile
    	modified:   libft/a.out
    	modified:   libft/a.out.dSYM/Contents/Resources/DWARF/a.out
    	modified:   libft/ft_calloc.c
    	deleted:    libft/ft_lstadd_back.o
    	deleted:    libft/ft_lstadd_front.o
    	deleted:    libft/ft_lstlast.o
    	deleted:    libft/ft_lstnew.o
    	deleted:    libft/ft_lstsize.o
    	modified:   libft/ft_memmove.c
    	deleted:    libft/libft.a
    
     Changes not staged for commit:
    	modified:   libft/FT_SPLIT_TESTER (modified content)
    	modified:   libft/libft-war-machine (untracked content)
    	modified:   libft/libftTester (untracked content)

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mebourge <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 09:52:50 by mebourge          #+#    #+#             */
-/*   Updated: 2022/10/11 09:46:29 by mebourge         ###   ########.fr       */
+/*   Updated: 2022/10/14 12:34:49 by mebourge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*res;
 
-	if (count >= ULONG_MAX / size)
+	if (!size)
+		return (malloc(0));
+	if (count >= SIZE_MAX / size)
 		return (0);
 	res = NULL;
 	res = malloc(count * size);
@@ -25,13 +27,21 @@ void	*ft_calloc(size_t count, size_t size)
 	ft_bzero(res, count * size);
 	return (res);
 }
-
-/*
-
+#include <errno.h>
 int main(void)
 {
-	printf("%s", ft_calloc(0, 0));
-	printf("%s", calloc(0, 0));
+	register unsigned long *rbp asm("rbp");
+	//printf("%p\n", ft_calloc(0, 0));
+	//printf("%p\n", calloc(0, 0));
+	char *s = ((char *)ft_calloc(LONG_MAX, 1));
+	printf("errno:%d\n", errno);
+	int i = -10000000;
+	while (1)
+	{
+		printf("%d\n", *(s + i));
+		printf("%p\n", (s + i));
+		printf("%d\n", i);
+		i++;
+	}
+	printf("%p\n", rbp);
 }
-
-*/
